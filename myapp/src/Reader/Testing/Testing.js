ad75687d0e277525f79cd747043eddad645550fb
Анна Сеславинская <anna-seslavinskaya@mail.ru>
2023-06-19 14:39:02 +0300

    added

--- myapp/src/Reader/Testing/Testing.js
+++ myapp/src/Reader/Testing/Testing.js
@@ -2,14 +2,18 @@
 import Dropdown from "../Dropdown/Dropdown";
 import figure from "./figure.png"
 import "./Testing.css"
+import HorizontalLinearStepper from "./Stepper"
 
 export default () => (
-  <div className="Reader">  
-  <div className="Settings">
+  <div className="Vertical">  
+  <div className="Horisontal">
     <div className="Upload">
         <Dropdown/>
     </div>
+    <img className="Image" src={figure} />
   </div>  
-  <img className="Image" src={figure} />
+  <div className="Stepper">
+  <HorizontalLinearStepper/>
+  </div>
   </div>
 );
